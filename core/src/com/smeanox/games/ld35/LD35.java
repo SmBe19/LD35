b54cf9c59f77b65640c8f060290e8ef3334e46be
Matteo Signer <matteo.signer@gmail.com>
2016-04-18 16:22:40 +0200

    main menu

--- core/src/com/smeanox/games/ld35/LD35.java
+++ core/src/com/smeanox/games/ld35/LD35.java
@@ -7,11 +7,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.smeanox.games.ld35.screens.GameScreen;
+import com.smeanox.games.ld35.screens.MenuScreen;
 
 public class LD35 extends Game {
 
 	@Override
 	public void create() {
+		setScreen(new MenuScreen(this));
+	}
+
+	public void showGameScreen() {
 		setScreen(new GameScreen());
 	}
 
