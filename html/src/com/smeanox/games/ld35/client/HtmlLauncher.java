7c0dd8ff115773a4ca64e56be94d9c3b6472df80
SmBe19 <git@schmidb.ch>
2016-04-19 04:06:17 +0200

    Web Export.

--- html/src/com/smeanox/games/ld35/client/HtmlLauncher.java
+++ html/src/com/smeanox/games/ld35/client/HtmlLauncher.java
@@ -3,13 +3,14 @@ package com.smeanox.games.ld35.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.smeanox.games.ld35.Consts;
 import com.smeanox.games.ld35.LD35;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+                return new GwtApplicationConfiguration((int) (Consts.WIDTH * Consts.DESKTOP_SCALE), (int) (Consts.HEIGHT * Consts.DESKTOP_SCALE));
         }
 
         @Override
